30f9e9976a71e9a63ca0171fb47210ddc2d3c19a
Tito Hinostroza <tehh2@DESKTOP-IQ840EC>
2022-06-27 23:59:41 -0500

    Sin cambios

--- temp/NewFile1.pas
+++ temp/NewFile1.pas
@@ -1,13 +1,14 @@
-TYPE
-  TAStr = [3]char;
-//  TAByte = [4]byte;
-//  tarr2 = array[] of char;
-//const 
-//  CCC: TAstr = (1,2,3);
-VAR  
-  str : TAStr = ('a','b','c'); 
-  str2 : TAStr = ['a','b','c']; 
-//  numb: TAByte = (1, 2, 3); //ERROR: Expected 4 items.
-//  aaa: array[] of char = 'abcds';
+{Description of the program.}
+procedure ChrOUT(c: char);
 begin
-end. 
+end;
+
+var
+k : byte;
+begin
+ChrOUT('H');
+repeat 
+  ChrOUT('H'); // this is not printed
+until true; 
+//asm RTS end
+end.
