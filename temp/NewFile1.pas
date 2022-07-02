50b7adc1740eadf94a1ea51c419ed17750bb0921
Tito Hinostroza <tehh2@DESKTOP-IQ840EC>
2022-07-01 23:00:14 -0500

    Modo release

--- temp/NewFile1.pas
+++ temp/NewFile1.pas
@@ -1,20 +1,16 @@
 {"Hello World" P65Pas program.}
-program Hello;
 uses Commodore64;
 var
-a : array[4] of char; // 5 instead of 4 or the space is not printed
-i : byte;
-e : char;
+  a : array[4] of char; // 5 instead of 4 or the space is not printed
+  i : byte;
 begin
-a[0] := 'H';
-a[1] := 'O';
-a[2] := 'L';
-a[3] := 'A';
-
-for i := 0 to 3 do
-  ChrOUT(a[i]); 
-end;
-
-
-asm RTS end
+  a[0] := 'H';
+  a[1] := 'O';
+  a[2] := ' ';
+  a[3] := 'A';
+  
+  for i := 0 to 3 do
+    ChrOUT(a[i]); 
+  end;
+  asm RTS end
 end.
